9e25a3e4fcf4a8bee04fbddd42e695923cff933b
Tuan Hoang <uthtuan0204@gmail.com>
2025-10-31 20:54:34 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-3xl font-bold underline'>
+        Hello Vite + React!
+      </div>
+    </div>
+  )
+}
+
+export default App
